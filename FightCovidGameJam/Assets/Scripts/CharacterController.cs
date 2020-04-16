c36d3f33fa193643f1bc3f499688da69d2627680
MarcGuillen <marc.guillen.96@gmail.com>
2020-04-16 15:33:36 +0200

    started character controller

---
+++ FightCovidGameJam/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float distance = 50f;
+
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            //create a ray cast and set it to the mouses cursor position in game
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                //draw invisible ray cast/vector
+                Debug.DrawLine(ray.origin, hit.point);
+                //log hit area to the console
+                if (IsWalkable(hit.point))
+                    MoveToDestination(hit.point);
+            }
+        }
+    }
+
+    bool IsWalkable(Vector3 point)
+    {
+        return NavMesh.CalculatePath(transform.position, point, 0, null);
+    }
+
+    void MoveToDestination(Vector3 destination)
+    {
+        agent.SetDestination(destination);
+    }
+}
