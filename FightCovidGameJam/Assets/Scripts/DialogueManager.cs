33fab4d50456c20d1e317eae9eb8999ef7163ad1
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-04-16 17:48:41 +0200

    Added UI basics

---
+++ FightCovidGameJam/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject myDialogueGO;
+    public GameObject NPCDialogueGO;
+    Text myDialogueText;
+    Text NPCDialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDialogueText = myDialogueGO.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetText(string text)
+    {
+        myDialogueText.text = text;
+    }
+}
