b3513ede3a69cc725de177e90246c1a187fb6ae1
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2020-04-18 15:59:25 +0200

    day 15

--- FightCovidGameJam/Assets/Scripts/GameManager.cs
+++ FightCovidGameJam/Assets/Scripts/GameManager.cs
@@ -70,6 +70,7 @@ public class GameManager : MonoBehaviour
             boolean_stats.Add("Shop", false);
             boolean_stats.Add("Plant", false);
             boolean_stats.Add("Went_Out", false);
+            boolean_stats.Add("Doctor_Out", false);
 
             action_manager = GetComponent<ActionManager>();
             fade = GameObject.FindGameObjectWithTag("Fade");
